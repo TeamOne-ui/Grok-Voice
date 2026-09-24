61958b6f2953d4da723eb570740255247e38a6aa
TeamOne-ui <ryancrossette@gmail.com>
2026-09-23 22:51:56 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',height:'100vh',background:'#0a0a0a',color:'#fff',fontFamily:'sans-serif'}}>
+      <h1>Grok Voice</h1>
+      <p>Coming soon</p>
+    </main>
+  );
+}
